f08deb2cb43e2333b600250c4c46f75591565967
Humayun Kabir <humayun1729@gmail.com>
2025-12-12 07:54:12 +0600

    class method added

--- classes/example1.js
+++ classes/example1.js
@@ -1,1 +1,14 @@
-ggit 
+class myClass {
+    constructor() {
+
+    }
+
+    method1() {};
+    method2() {};
+    method3() {};
+
+    .........
+}
+// This is the basic format of class
+// in javascript
+
